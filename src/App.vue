633fb924f1d93c7199c22fdc92f0c9ce8c78f495
indra120 <indrapranata120@gmail.com>
2024-02-07 15:08:52 +0800

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
 <template>
   <Header />
+
+  <main></main>
 </template>
 
 <script setup lang="ts">
 import Header from "@/components/Header.vue"
 </script>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  min-height: calc(100vh - 55px);
+  max-width: 480px;
+  padding: 16px;
+  margin: 0 auto;
+  background-color: var(--main-container-color);
+}
+</style>
