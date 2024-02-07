fa89674f9cbb3b7ca9c8e1f346c1850c70e37404
indra120 <indrapranata120@gmail.com>
2024-02-07 15:06:05 +0800

    Header

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,7 @@
 <template>
-  <div>Hi!</div>
+  <Header />
 </template>
+
+<script setup lang="ts">
+import Header from "@/components/Header.vue"
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,43 @@
+<template>
+  <header>
+    <img :src="emojiIcon" alt="emoji icon" />
+    <img :src="searchIcon" alt="search icon" />
+    <span> EmojiSearch </span>
+  </header>
+</template>
+
+<script setup lang="ts">
+import searchIcon from "@/assets/search-icon.svg"
+import emojiIcon from "@/assets/emoji-icon.svg"
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  background-color: var(--main-background-color);
+  padding: 0 16px;
+  height: 55px;
+}
+
+span {
+  color: var(--secondary-font-color);
+  font-size: 24px;
+  font-weight: bold;
+}
+
+img {
+  height: 32px;
+  width: 32px;
+}
+
+img:nth-child(1) {
+  margin-right: 8px;
+}
+
+img:nth-child(2) {
+  margin-top: 3px;
+}
+</style>
